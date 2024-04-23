77c3223e1f2074ae260a917f8111e45222209e82
Régis Meyssonnier <67794100+regismeyssonnier@users.noreply.github.com>
2024-04-23 19:57:39 +0200

    Add files via upload

--- acp.py
+++ acp.py
@@ -39,21 +39,24 @@ ax.scatter(means[0], means[2], means[1], color='r', s=100, label='Moyennes', mar
 
 min_length = 1
 max_length = 10
+col = ['green','orange', 'black']
+axis=['X','Y','Z']
 # Afficher les vecteurs propres à partir des moyennes
+num = [0,2,1]
 for i in range(len(eigenvalues)):
-    length = np.sqrt(eigenvalues[i])
+    length = np.sqrt(eigenvalues[num[i]])
     scaled_length = min_length + (max_length - min_length) * (length - min(eigenvalues)) / (max(eigenvalues) - min(eigenvalues))
     ax.quiver(means[0], means[2], means[1],
               eigenvectors[0, i], eigenvectors[2, i], eigenvectors[1, i],
               length=scaled_length, normalize=True,
-              color='g', label=f'Eigenvalue {i+1}')
+              color=col[i], label=axis[num[i]])
 
 
 # Ajouter des étiquettes et un titre au graphique
 ax.set_xlabel('Variable X')
 ax.set_ylabel('Variable Z')
 ax.set_zlabel('Variable Y')
-ax.set_title('Données et vecteurs propres de la matrice de covariance')
+ax.set_title('Donn\xe9es et vecteurs propres de la matrice de covariance')
 
 # Afficher la légende
 ax.legend()
