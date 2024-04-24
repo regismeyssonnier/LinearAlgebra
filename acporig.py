64fb36e8a4ae23a343e1e02834678e38636c0bcb
Régis Meyssonnier <67794100+regismeyssonnier@users.noreply.github.com>
2024-04-24 16:17:56 +0200

    Add files via upload

--- acporig.py
+++ acporig.py
@@ -1,16 +1,17 @@
+
 # coding: latin
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D  # Importer les outils 3D
 
-# Exemple de données (n observations, p variables)
+# Exemple de donnÃ©es (n observations, p variables)
 #data = np.array([[1, 2, 3],
 #                 [4, 5, 6],
 #                 [7, 8, 9],
 #                 [10, 8, 12]])
 
 
-# Définir les dimensions des données (n observations, p variables)
+# DÃ©finir les dimensions des donnÃ©es (n observations, p variables)
 n = 50  # Nombre d'observations
 p = 3    # Nombre de variables
 mx_length = 10.0 / 2.0
@@ -20,7 +21,7 @@ data = np.random.randn(n, p)*mx_length*2.0
 # Calculer la moyenne de chaque variable
 means = np.mean(data, axis=0)
 
-# Centrer les données (soustraire la moyenne de chaque variable)
+# Centrer les donnÃ©es (soustraire la moyenne de chaque variable)
 centered_data = data - means
 
 # Calculer la matrice de covariance
@@ -35,38 +36,38 @@ print("Eigen Values:\n", eigenvalues)
 print("Eigen Vector:\n", eigenvectors)
 
 
-# Créer une figure 3D pour afficher les données et les vecteurs propres
+# CrÃ©er une figure 3D pour afficher les donnÃ©es et les vecteurs propres
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111, projection='3d')
 
-# Afficher les points de données d'origine
-ax.scatter(data[:, 0], data[:, 2], data[:, 1], color='b', label='Donn\xe9es')
+# Afficher les points de donnÃ©es d'origine
+ax.scatter(data[:, 0], data[:, 1], data[:, 2], color='b', label='Donn\xe9es')
 
 # Afficher les moyennes de chaque variable
-ax.scatter(means[0], means[2], means[1], color='r', s=100, label='Moyennes', marker='o')
+ax.scatter(means[0], means[1], means[2], color='r', s=100, label='Moyennes', marker='o')
 
-min_length = 0.5
+min_length = 1
 max_length = mx_length
 col = ['green','orange', 'black']
-axis=['X','Z','Y']
-# Afficher les vecteurs propres à partir des moyennes
-num = [0,2,1]
+axis=['X','Y','Z']
+# Afficher les vecteurs propres Ã  partir des moyennes
+num = [0,1,2]
 for i in range(len(eigenvalues)):
     length = np.sqrt(eigenvalues[num[i]])
     scaled_length = min_length + (max_length - min_length) * (length - min(eigenvalues)) / (max(eigenvalues) - min(eigenvalues))
-    ax.quiver(means[0], means[2], means[1],
-              eigenvectors[0, num[i]], eigenvectors[2, num[i]], eigenvectors[1, num[i]],
+    ax.quiver(means[0], means[1], means[2],
+              eigenvectors[0, num[i]], eigenvectors[1, num[i]], eigenvectors[2, num[i]],
               length=scaled_length*scale, normalize=True,
               color=col[num[i]], label=axis[num[i]])
 
 
-# Ajouter des étiquettes et un titre au graphique
+# Ajouter des Ã©tiquettes et un titre au graphique
 ax.set_xlabel('Variable X')
-ax.set_ylabel('Variable Z')
-ax.set_zlabel('Variable Y')
+ax.set_ylabel('Variable Y')
+ax.set_zlabel('Variable Z')
 ax.set_title('Donn\xe9es et vecteurs propres de la matrice de covariance')
 
-# Afficher la légende
+# Afficher la lÃ©gende
 ax.legend()
 
 # Afficher le graphique 3D
